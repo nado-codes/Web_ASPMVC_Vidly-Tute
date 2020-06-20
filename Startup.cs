1ef00988f1404bea55f366a626d20bb66dec5aff
nathan.linsley960731 <nathan.linsley960731@hotmail.com>
2020-06-21 01:34:31 +1000

    Initial Commit v2

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPTute_Vidly.Startup))]
+namespace ASPTute_Vidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
